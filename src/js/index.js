cf00b7fa6b3aed49ee218309782801e47c9f95a9
Hawty1 <marius_erpenbach@web.de>
2021-04-26 15:55:22 +0200

    added js

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+const addTask =()=>{
+// create new section
+var newTaskSection = document.createElement("section");
+// create p with the user input 
+var newTaskPhrase = document.createElement("p");
+// get user input
+newPhrase = document.getElementById("newTask").value;
+newTaskPhrase.innerHTML=newPhrase;
+//append the p to the section
+newTaskSection.appendChild(newTaskPhrase);
+// append section to task list
+document.getElementById("toDoList").appendChild(newTaskSection)
+}
