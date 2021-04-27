524f1b5cc8fa95865643e52d19c73166467e1cf6
Hawty1 <marius_erpenbach@web.de>
2021-04-27 06:16:25 +0200

    random colors for new added sections

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,30 @@
-const addTask =()=>{
-// create new section
+numberOfTasks = 0;
+const addTask =(e)=>{
+    const randomColor=()=>{
+        // return number random number between 0 and 100 and then add 155 to it. (3times) to generate smooth light colors.
+        newRGBvalues=[155,155,155];
+        for (i=0;i<=3;i++){
+        newRGBvalues[i]+=(Math.floor(Math.random() * 101))
+        }
+        return `rgb(${newRGBvalues[0]},${newRGBvalues[1]},${newRGBvalues[2]})`
+    }
+    
+    e.preventDefault();
+// ----------------------------------------------------create new section
 var newTaskSection = document.createElement("section");
-// create p with the user input 
+newTaskSection.style.cssText += `background-color:${randomColor()}`;
+// newTaskSection.setAttribute("id",`${numberOfTasks}-addedTask`)
+// ----------------------------------------------------create p with the user input 
 var newTaskPhrase = document.createElement("p");
-// get user input
+// -------------------------------------------------------get user input
 newPhrase = document.getElementById("newTask").value;
 newTaskPhrase.innerHTML=newPhrase;
-//append the p to the section
+//---------------------------------------------append the p to the section
 newTaskSection.appendChild(newTaskPhrase);
-// append section to task list
-document.getElementById("toDoList").appendChild(newTaskSection)
+// -----------------------------------------append section to task list
+document.getElementById("toDoList").insertBefore(newTaskSection,newTaskForm)
+numberOfTasks++;
 }
+
+
+document.querySelector("form").addEventListener("submit", addTask);
