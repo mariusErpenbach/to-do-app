52372546dbd47c67fd51a0069399472906b3eba0
Hawty1 <marius_erpenbach@web.de>
2021-04-27 10:29:12 +0200

    Updates

--- js/index.js
+++ js/index.js
@@ -1,13 +1,40 @@
-const addTask =()=>{
-// create new section
+numberOfTasks = 0;
+const addTask =(e)=>{
+    e.preventDefault();
+// ----------------------------------------------------create new section
 var newTaskSection = document.createElement("section");
-// create p with the user input 
+newTaskSection.style.cssText += `background-color:${randomColor()}`;
+// newTaskSection.setAttribute("id",`${numberOfTasks}-addedTask`)
+// ----------------------------------------------------create p with the user input 
 var newTaskPhrase = document.createElement("p");
-// get user input
-newPhrase = document.getElementById("newTask").innerHTML;
+// -------------------------------------------------------get user input
+newPhrase = document.getElementById("newTask").value;
 newTaskPhrase.innerHTML=newPhrase;
-//append the p to the section
+//---------------------------------------------append the p to the section
 newTaskSection.appendChild(newTaskPhrase);
-// append section to task list
-document.getElementById("toDoList").appendChild(newTaskSection)
+//---------------------------------------------create checkbox for done tasks
+var newCheckbox = document.createElement("input");
+newCheckbox.setAttribute("type","checkbox");
+//---------------------------------------------append checkbox to section;
+newTaskSection.appendChild(newCheckbox);
+// -----------------------------------------append section to task list
+document.getElementById("toDoList").insertBefore(newTaskSection,newTaskForm)
+numberOfTasks++;
+// -----------------------------------------------check if page is full 
+if(numberOfTasks==8){
+// -----------------------------------------------Delete form
+  document.getElementById("toDoList").removeChild(newTaskForm)
 }
+}
+
+  
+const randomColor=()=>{
+    // return number random number between 0 and 100 and then add 155 to it. (3times) to generate smooth light colors.
+    newRGBvalues=[155,155,155];
+    for (i=0;i<=3;i++){
+    newRGBvalues[i]+=(Math.floor(Math.random() * 101))
+    }
+    return `rgb(${newRGBvalues[0]},${newRGBvalues[1]},${newRGBvalues[2]})`
+}
+
+document.querySelector("form").addEventListener("submit", addTask);
